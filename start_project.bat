c3257f6e74ccc91411398a1b86741494c74ec027
JakubSzwajka <szwajkajakub@gmail.com>
2021-03-18 10:32:28 +0100

    description added

--- start_project.bat
+++ start_project.bat
@@ -1,8 +1,15 @@
 @echo off
+
+@REM create a folder and go into it
 mkdir %1 
 cd %1
+
+@REM some python related stuff
 python -m venv venv
 
+@REM git related stuff
 git init 
 echo **/venv > .gitignore
+
+@REM project description stuff
 echo #%1 > readme.md
